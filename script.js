8064420acfcce9575e7eb723256569ab0009f344
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2024-04-05 11:24:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.querySelector(".container");
+
+    // Generate 800 square elements
+    for (let i = 0; i < 800; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+    }
+});
